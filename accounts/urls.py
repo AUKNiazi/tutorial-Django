86b9dfe48943076d57ad371122d6670ebb106420
Asmat Ullah Khan <61291452+AUKNiazi@users.noreply.github.com>
2021-05-11 01:10:28 +0500

    end of 8th lec

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,15 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     #r means regular ^$ meand stop. it is same as writing url('', views.home)  
-    url(r'^$', views.home),
-
+    path('', views.home),
+    #url('login/', login, {'template_name': 'accounts/login.html'})
+    path('login/', 
+        LoginView.as_view(
+            template_name='accounts/login.html'
+        ), 
+        name="login"
+    ),
 ]
