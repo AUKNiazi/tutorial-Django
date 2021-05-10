44356254a9df5d84c5692a56d381ab7a1621c1d8
Asmat Ullah Khan <61291452+AUKNiazi@users.noreply.github.com>
2021-05-10 16:26:43 +0500

    7th lecture ended

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,1 +1,8 @@
 from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    #r means regular ^$ meand stop. it is same as writing url('', views.home)  
+    url(r'^$', views.home),
+
+]
